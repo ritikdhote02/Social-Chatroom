e6547524ffe568ff823a87d7d78d1cbc80d89357
Ritik Dhote <ritikdhote0b@gmail.com>
2024-12-06 23:20:48 +0530

    vercel

--- chatappprj/settings.py
+++ chatappprj/settings.py
@@ -28,10 +28,7 @@ SECRET_KEY = 'confkey'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [   '*',
-                    '127.0.0.1', 
-                    'socialchatroom.herokuapp.com',
-                    '.herokuapp.com']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
@@ -80,12 +77,15 @@ WSGI_APPLICATION = 'chatappprj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# uncomment it for running in local
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES = {}
 
 
 # Password validation
